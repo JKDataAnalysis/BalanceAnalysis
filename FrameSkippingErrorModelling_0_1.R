bb90eada3a1a4fba2b89839b8a3d9410e3ab99a9
J Kelly <jon.kelly@tuta.io>
2024-07-03 18:12:53 +0100

    Updated development notes, frame skipping analysis guide and script

--- FrameSkippingErrorModelling_0_1.R
+++ FrameSkippingErrorModelling_0_1.R
@@ -17,18 +17,17 @@
 # * Searches files for those with no skipped frame and then uses these as a
 #     criterion data set and generates model data sets from these with 
 #     iteratively more frames removed
+# * Remove blocks of frames rather than always single as skipped frames are
+#   clustered in the Tekscan data. This is based on gap counts from files used 
+#   for generating gap profiles.
 #
 # PART 2: Analyse Errors
 # * Calculates the errors between the criterion data sets and their derived 
 #     files with frames removed (coding ain't pretty though!)
 # * Calculates % errors
 # * Plot mean/ SD error against number of frames removed for each variable
-# [Done] Export considerably more data and use more realistic (larger) number of
-#   dropped frames for model data. This will give indication of;
-#   1) whether the current plots are sensible for the data [No]
-#   2) what regression model to use
-# [TODO] Determine the regression model to be used. Is linear sufficient?
-# [TODO] Calculate regression formulae for error by number of missing frames for
+# * Plots modified to return path length, range and ellipse as separate plots
+# * Calculate regression formulae for error by number of missing frames for
 #     each variable grouped by interp == T/F
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -36,9 +35,10 @@ library(readr) # read_csv
 library(reshape2) # Used for reshaping df before plotting
 library(ggplot2)
 library(magrittr) # used for pipes
+library(dplyr)
 
 # GENERATE MODEL DATA ==========================================================
-generate_model_data <- function(gap_mean, gap_sd, gap_max){
+generate_model_data <- function(gpmean = gap_mean, gpsd = gap_sd, gpmax = gap_max, gpcounts = gap_counts){
   # Just pull all settings in for simplicity though most of analysis setting won't be used
   settings <- get_setns() # Get settings
   file_list <- get_data_files(settings$file_type)
@@ -50,20 +50,20 @@ generate_model_data <- function(gap_mean, gap_sd, gap_max){
       dlg_message("All files selected have missing frames, unable to proceed.", type = "ok")
       break
     }
-    ### Clean file name for labelling -----
+  ### Clean file name for labelling --------------------------------------------
     # Remove file type
     clean_file_name <- sub("([^.]+)\\.[[:alnum:]]+$", "\\1", basename(file_i) )
     # Replace white space
     clean_file_name <- gsub("\\s", "_", clean_file_name)
     file_inf <- list(file_name = clean_file_name) # Start file info list with file name
-    
-    ### Read data file ----------------
+
+  ### Read data file -----------------------------------------------------------
     if (file.access(file_i, mode = 4) == 0) { # If can read from file
       CoF_data <- read_txt_data(
         file_i,
         delm = settings$delim,
         skp = settings$skip,
-        maxn = settings$maxn, 
+        maxn = settings$maxn,
         c_type = as.character(settings$colm_type),
         c_names = as.character(
           strsplit(settings$colm_names, ",")[[1]] # Split string to vector by commas
@@ -74,37 +74,42 @@ generate_model_data <- function(gap_mean, gap_sd, gap_max){
         file_list <- file_list[! file_list %in% file_i]
       } else { # Read file OK- carry on
         
-        ### Read sample rate -----------------
+        ### Read sample rate ---------------------------------------------------
         sr <- read_sample_rate(file_i, settings$sr_skp, settings$sr_colm) # Read in sample rate
-        
+
         ### Check if file has missing rows -------------------------------------
         if (nrow(CoF_data) != (max(CoF_data$Time) - min(CoF_data$Time)) * sr  + 1) {
           print(paste(clean_file_name, "has missing rows. Skip"))
         } else {
           print(paste("Generating data sets from", clean_file_name))
-          
+          crit_row <- nrow(CoF_data) # record number of rows present to start with
+
           ### Generate list of times to remove at random -----------------------
           # Set seed based on millisecond system time so won't always remove the same times
           set.seed(as.numeric(substr(format(Sys.time(), "%OS6"), 4, 10)))
-          # remove_times <- round(rnorm(gap_max * 2, mean = gap_mean, sd = gap_sd), 2) %>%
-            # .[. >= settings$start_t & . <= settings$end_t] # Remove values outside of time window
-          
+
           ### Make duplicate files with rows removed ---------------------------
           CoF_data$Time <- round(CoF_data$Time, 2) # Round to 2dp as sample rate not exact
-          for (g in 0:gap_max){
+          while (crit_row - nrow(CoF_data) < gpmax) {
             flname <- paste(
-              save_dir, "/",clean_file_name, 
-              "_", formatC(g, width = 3, flag = "0"), 
-              ".csv", 
+              save_dir, "/",clean_file_name,
+              "_", formatC(crit_row - nrow(CoF_data), width = nchar(gpmax), flag = "0"),
+              ".csv",
               sep = ""
               )
             write_excel_csv(as.data.frame(sr), flname)
             write_excel_csv(CoF_data, flname, append = T, col_names = T)
             matcht <- FALSE
-            while(matcht == FALSE){
-              t <- round(rnorm(1, mean = gap_mean, sd = gap_sd), 2) # Generate pseudo-random time
-              if(nrow(subset(CoF_data, Time == t) ) == 1){ # found matching time
-                CoF_data <- subset(CoF_data, Time != t) # Remove row
+            while(matcht == FALSE){ # Keep going till find a time that exists in the file
+              begint <- round(rnorm(1, mean = gpmean, sd = gpsd), 2) # Generate pseudo-random time
+              if(nrow(subset(CoF_data, Time == begint) ) == 1){ # found matching time
+                # Set the cut end time to...
+                # the frame gap corresponding to the lowest cumulative sum that is greater than the random number chosen...
+                endt <- subset(gpcounts, cumcount > sample(1:(max(gpcounts$cumcount) - 1), 1))[, "fg"][1] %>%
+                  subtract(1) %>% # subtract 1 since frame gap of 2 means 1 frame removed
+                  multiply_by(1 / sr) %>% # multiplied by the sample rate..
+                  add(begint) # plus the cut start time
+                CoF_data <- subset(CoF_data, Time < begint | Time >= endt) # Remove row(s)
                 matcht <- TRUE
               }
             }
@@ -113,7 +118,7 @@ generate_model_data <- function(gap_mean, gap_sd, gap_max){
       }
     }
   }
-  Print("Finished generating model files")
+  print(">>>>>>> Finished generating model files <<<<<<<")
         
 }
 
@@ -158,22 +163,16 @@ get_dir <- function(){
 
 run_err_analysis <- function(x){
   # Return error calcs and plots as global variable so available for further analysis/ mods
-  print("Calculating errors....")
+  print("Initiatiing error calculations")
   assign("err", calc_frame_skip_error(x), envir = .GlobalEnv) 
+  print("Error calculations created as a data frame called 'err'")
   print("Plotting errors....")
-  assign("err_plots", plot_errors(err), envir = .GlobalEnv) 
-  show(err_plots) # Show the plots
+  plot_errors(err)
+  print("Error plots created as 'plot_Range', 'plot_pl' and 'plotEllipse'")
   print("Calculating regression formulae....")
-  # first version using interp value (would need to be within for loop)
-  # err %>% 
-  #   group_by(Interp) %>% 
-  #   apply(.[ , 18:26], 2, calc_regr, x = .$missn_frames)
-  # # apply(err[ , 18:26], 2, calc_regr, x = err$missn_frames, s = err$Interp, s_val = T)
-  
-  # 2nd version using grouping
-  # err %>% 
-  # {if(length(unique(.$Interp)) > 1) group_by(., Interp) else .} %>%
-  #   group_modify(~ apply(err[ , 18:26], 2, calc_regr, x = err$missn_frames) )
+  assign("regr", regr_mdl(err), envir = .GlobalEnv)
+  print("Linear regression coefficients and R^2 values created as a data frame called 'rerg'")
+  print(">>>>>>> Error analysis complete <<<<<<<<<")
 }
 
 ## Calculate Error Caused by Missing Rows ======================================
@@ -182,7 +181,7 @@ calc_frame_skip_error <- function(x){
   # Also finds differences between criterion and skp trial trials but since
   # trial is numeric and crit trial = 0, this doesn't matter (I did say it was a
   # kludge!)
-  df <- data.frame() # Create empty data frame
+  err_df <- data.frame() # Create empty data frame
     inf_colms <- c(1:3, 5:7) # Columns with file info
     gap_colms <- c(17:28) # Columns with frame gap data
     
@@ -200,45 +199,54 @@ calc_frame_skip_error <- function(x){
     names(perc_errs) <- paste(names(perc_errs), "_%", sep="")  # Append % column names
     errs$No_frames <- errs$No_frames * -1 # Invert differences, i.e. make No frames removed
     # Reinstate columns not included in diff calcs, drop row names
-    err_df <- cbind(subset(x, as.integer(trial) != 0)[inf_colms], errs, perc_errs, row.names = NULL) %>% 
-      rbind(df, .)
+    err_df <- cbind(errs, perc_errs, row.names = NULL) %>% 
+      rbind(err_df, .)
   }
-  return(err_df)
+    cbind(subset(x, as.integer(trial) != 0)[inf_colms], err_df)
 }
   
 ## Plot Errors =================================================================  
 plot_errors <- function(df){
-  # Form all % error variables into a single column
-  dfm <-  melt(df[c(3, 6, 7, 17:25)], id.vars = c("Interp", "No_frames", "trial"))
+  idc <- c("Interp", "No_frames", "trial") # list of id columns
   
-  ggplot(data = dfm, mapping = aes(x = No_frames, y = value, colour = variable)) + 
-    geom_point() +
-    geom_smooth(formula = y ~ x, method = "lm") +
-    labs(x = "number of frames removed", y = "% error") +
-    facet_grid(cols = vars(Interp))
+  # melt all % error columns into a single column with idc as ID columns
+  dfm <-  names(df)[grepl("_%", names(df))] %>% 
+    c(., idc) %>% 
+    df[.] %>% 
+    melt(., id.vars = idc)
+  
+  for (i in c("_Range", "_pl", "Ellipse")){
+    p <- 
+      dfm %>% 
+      filter(grepl(i, variable)) %>%
+      ggplot(data = ., mapping = aes(x = No_frames, y = value, colour = variable)) +
+        geom_point() +
+        geom_smooth(formula = y ~ x, method = "lm") +
+        labs(x = "number of frames removed", y = "% error") +
+        facet_grid(Interp ~ variable)
+      show(p)
+      assign(paste("plot", i, sep = ""), p, envir = .GlobalEnv)
+  }
 }
 
-## Calculate Regression Lines ==================================================
+## Calculate Regressions =======================================================
 # See https://www.statology.org/polynomial-regression-r/
-calc_regr <- function(x, y) { #}, s, s_val){
-  lm(x ~ y) %>%  #, subset = s == s_val)
-    as.data.frame()
+regr <- function(x, y, i, tf){
+  m <- lm(x~y -1, subset = (i == tf))
+  as.data.frame(cbind(coeff = coefficients(m), r_sqr = summary(m)$r.squared))
 }
-#  
-#   gap_data <- subset(model_data, trial != 0) %>% 
-#     group_by(file_name) #%>% 
-#     # print()
-#     browser()
-#     apply(gap_data[, -1:-3], 2, sum)
-#   # Value = value - value from above
-#     
-#     # subset(model_data, (trial == 0, file_name == ))
-#   
-#   # Then do calculations on errors NOT original data
-#   model_data %>% 
-#     group_by(file_name) %>% 
-#     group_by(Interp) %>% 
-#     group_by(trial) %>% # trial = number of frames removed
-#     summarise(n = n(), meanCoFxRange = mean(CoF_x_Range), SDCoFxRange = sd(CoF_x_Range))
-#   
-# }
+
+## Calculate Reg for all =======================================================
+regr_mdl <- function(err){
+  for (i in c(TRUE, FALSE)) {
+    e <- err %>% 
+      select(c(8:16)) %>% 
+      apply(., 2, regr, y = err$No_frames, i = err$Interp, tf = i) %>% 
+      unlist() %>% 
+      as.data.frame() #%>% 
+    assign(paste("e", i, sep = "_"), e)
+  }
+  cbind(e_TRUE, e_FALSE) %>% 
+    set_colnames(c("Interp", "No_Interp"))
+}
+
